6d62c3ba247f79d7fb756a33383602593fdf8869
Arturo Carcamo <34602460+Arturo7C@users.noreply.github.com>
2020-06-18 22:56:45 -0400

    Small Update to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,17 +10,16 @@ const Footer = () => {
       <a
         className="badge badge-dark"
         rel="noopener"
-        href="https://github.com/hashirshoaeb"
+        href="https://github.com/Arturo7C"
         aria-label="My GitHub"
       >
-        Hashir Shoaib
+        Arturo Carcamo
       </a>{" "}
       using <i className="fab fa-react"></i>
       <p>
         <small className="text-muted">
           {" "}
-          Project code is open source. Feel free to fork and make your own
-          version.
+          
         </small>
       </p>
     </footer>
