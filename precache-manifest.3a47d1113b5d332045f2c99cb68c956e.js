b6825e23fb8a601fd8f2c0feae8f0a62d83a877c
Arturo Carcamo <34602460+Arturo7C@users.noreply.github.com>
2020-06-18 20:14:46 -0400

    Updates

---
+++ precache-manifest.3a47d1113b5d332045f2c99cb68c956e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e934c459b1955c1a85f5cacf04060ad",
+    "url": "/Arturo.Carcamo/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/Arturo.Carcamo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f78a50a8bec80e073811",
+    "url": "/Arturo.Carcamo/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/Arturo.Carcamo/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/Arturo.Carcamo/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78a50a8bec80e073811",
+    "url": "/Arturo.Carcamo/static/js/main.8d26afd1.chunk.js"
+  },
+  {
+    "revision": "7ef9c8be97a693999be8",
+    "url": "/Arturo.Carcamo/static/js/runtime-main.7fc6e234.js"
+  },
+  {
+    "revision": "52ab9ed1b78b77e3579ae8481ce89d84",
+    "url": "/Arturo.Carcamo/static/media/resume.52ab9ed1.pdf"
+  }
+]);
